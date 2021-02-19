b7e86365a3d5caa438875bd6ffe560a0b9a9316f
francoponce16 <67753283+francoponce16@users.noreply.github.com>
2021-02-19 15:53:48 -0300

    Add files via upload

---
+++ Franco-Ponce-guia-6/Guia 6/Ej 2/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int i=0,ng=0,acu=0,num=0;
+	float pro=0;
+	while (15>=i)
+	{
+		printf("\n Ingrese un numero ");
+		scanf("%d",&num);
+		acu=acu+num;
+		if(num>ng){
+			ng=num;
+		}
+		i=i+1;
+	}
+	pro=acu/15;
+	printf("\n El numero promedio es %f",pro);
+	printf("\n El numero mas grande es %d",ng);
+	return 0;
+}
